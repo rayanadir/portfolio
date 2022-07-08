32b8269428356cbcd25ca857b2442619fbaa229d
rayanadir <rayan.dahmena@gmail.com>
2022-07-09 00:01:49 +0200

    update footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import '../footer/Footer.scss'
 
 const Footer = () => {
+  const year = new Date().getFullYear()
   return (
     <footer className='footer' id="footer">
-        <p>Email</p>
-        <p>Github</p>
+        <p className='footer__element'>©{year}, Rayan Dahmena</p>
+        <p className="footer__element">LinkedIn : <a href="https://www.linkedin.com/in/rayan-dahmena-5687b2244/" className='footer__link'>Rayan Dahmena</a></p>
+        <p className='footer__element'>Email : rayan.dahmena@gmail.com</p>
+        <p className='footer__element'>Github : <a href="https://github.com/rayanadir" className='footer__link'>https://github.com/rayanadir</a> </p>
     </footer>
   )
 }
