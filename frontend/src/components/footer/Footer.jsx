08006a8eab3e40d0702325719e9957c74b93bae8
rayanadir <rayan.dahmena@gmail.com>
2022-07-08 19:21:36 +0200

    add footer

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../footer/Footer.scss'
+
+const Footer = () => {
+  return (
+    <footer className='footer' id="footer">
+        <p>Email</p>
+        <p>Github</p>
+    </footer>
+  )
+}
+
+export default Footer
