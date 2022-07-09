7f1f4eb3c0828a258dcaa97c0c14596fb136008f
rayanadir <rayan.dahmena@gmail.com>
2022-07-09 20:31:13 +0200

    add stack component

---
+++ frontend/src/components/stack/Stack.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../stack/Stack.scss'
+
+const Stack = ({title, stackArr}) => {
+  return (
+        <article className="aboutme__stack" id='frontend'>
+          <h2 className="aboutme__stack__title">{title}</h2>
+          <ul className='aboutme__stack__list'>
+            {
+                stackArr.map((stack) => {
+                    return <li className='aboutme__stack__element' key={stack.id}>
+                            <img src={stack.icon} alt={stack.id} className='aboutme__stack__icon'/>
+                            <p className='aboutme__stack__name'>{stack.name}</p>
+                        </li>
+                })
+            }
+          </ul>
+        </article>
+  )
+}
+
+export default Stack
