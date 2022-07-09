9455fd17d123297c1cd871c8f7d0489f0e5983b1
rayanadir <rayan.dahmena@gmail.com>
2022-07-09 20:32:00 +0200

    add aboutme container component

--- frontend/src/containers/aboutme/AboutMe.jsx
+++ frontend/src/containers/aboutme/AboutMe.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import '../aboutme/AboutMe.scss'
+import React from 'react';
+import '../aboutme/AboutMe.scss';
+import { STACK } from '../../data/stack';
+import Stack from '../../components/stack/Stack';
 
 const AboutMe = () => {
 
@@ -18,6 +20,11 @@ const AboutMe = () => {
       <h3 className='aboutme__name'>Rayan Dahmena, {getAge()} ans</h3>
       <p className='aboutme__text'>J'ai suivi la formation de développeur Front-End d'OpenClassrooms. Mon objectif est d'obtenir un poste de développeur Front-End React.js.</p>
       <p className='aboutme__text'>Ce portfolio est accompagné de plusieurs projets professionnalisants, ainsi que d'autres fonctionnalités.</p>
+      <div className="aboutme__stack-container">
+        <Stack key="front" title="Stack Front-End" stackArr={STACK.filter((stack)=> stack.type==="front")} />
+        <Stack key="back" title="Stack Back-End" stackArr={STACK.filter((stack) => stack.type==="back")} />
+        <Stack key="other" title="... et autres" stackArr={STACK.filter((stack) => stack.type==="other")} /> 
+      </div>
     </section>
   )
 }
