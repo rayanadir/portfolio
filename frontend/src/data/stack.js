dbb9db7e95657f63c64f8cabd86bab6009e8fa24
rayanadir <rayan.dahmena@gmail.com>
2022-07-09 20:17:26 +0200

    add stack array

---
+++ frontend/src/data/stack.js
@@ -0,0 +1,108 @@
+import css from "../img/icons/css3.svg";
+import express from "../img/icons/expressjs.svg";
+import figma from "../img/icons/figma.svg";
+import git from "../img/icons/git.svg";
+import github from "../img/icons/github.svg";
+import html from "../img/icons/html5.svg";
+import js from "../img/icons/javascript.svg";
+import jest from "../img/icons/jest.svg";
+import mongo from "../img/icons/mongodb.svg";
+import node from "../img/icons/nodejs-icon.svg";
+import npm from "../img/icons/npm.svg";
+import react from "../img/icons/react-icon.svg";
+import redux from "../img/icons/redux.svg";
+import sass from "../img/icons/sass-icon.svg";
+import sqlite from "../img/icons/sqlite.svg"; 
+
+export const STACK =[
+    {
+        id:"html",
+        name:"HTML",
+        type:"front",
+        icon: html,
+    },
+    {
+        id:"css",
+        name:"CSS",
+        type:"front",
+        icon: css,
+    },
+    {
+        id:"sass",
+        name:"SASS",
+        type:"front",
+        icon: sass,
+    },
+    {
+        id:"js",
+        name:"JS",
+        type:"front",
+        icon: js,
+    },
+    {
+        id:"react",
+        name:"React",
+        type:"front",
+        icon: react,
+    },
+    {
+        id:"redux",
+        name:"Redux",
+        type:"front",
+        icon: redux,
+    },
+    {
+        id:"jest",
+        name:"Jest",
+        type: "front",
+        icon: jest,
+    },
+    {
+        id:"node",
+        name:"Node.js",
+        type:"back",
+        icon: node,
+    },
+    {
+        id:"mongo",
+        name:"MongoDB",
+        type:"back",
+        icon: mongo
+    },
+    {
+        id:"express",
+        type:"back",
+        name:"Express.js",
+        icon: express,
+    },
+    {
+        id:"sqlite",
+        name:"SQLite",
+        type:"back",
+        icon: sqlite,
+    },
+    {
+        id:"npm",
+        name:"NPM",
+        type:"other",
+        icon: npm
+    },
+    {
+        id:"git",
+        name:"Git",
+        type:"other",
+        icon: git,
+    },
+    {
+        id:"github",
+        name:"Github",
+        type:"other",
+        icon: github,
+    },
+    {
+        id:"figma",
+        name:"Figma",
+        type:"other",
+        icon: figma,
+    },
+]
