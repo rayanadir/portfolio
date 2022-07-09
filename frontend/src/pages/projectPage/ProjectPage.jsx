e9514e21579122bddf7ffcc99d52cf307e87902a
rayanadir <rayan.dahmena@gmail.com>
2022-07-09 22:43:41 +0200

    change component name

---
+++ frontend/src/pages/projectPage/ProjectPage.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../projectPage/ProjectPage.scss'
+
+const ProjectPage = ({
+  title,
+  description,
+  github_repo_link,
+  website_link,
+  main_image,
+  stack,
+  paragraphers,
+  other_links,
+  responsive,
+  images
+}) => {
+  /*const project = {title,
+    description,
+    github_repo_link,
+    website_link,
+    main_image,
+    stack,
+    paragraphers,
+    other_links,
+    responsive,
+    images}*/
+    //console.log(project);
+  return (
+    <main>
+      <section>
+        
+      </section>
+    </main>
+  )
+}
+
+export default ProjectPage
