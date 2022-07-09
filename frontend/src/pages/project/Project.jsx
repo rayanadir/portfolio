be08050dd0b40e16543d593b51cb44ff223cf132
rayanadir <rayan.dahmena@gmail.com>
2022-07-09 20:42:52 +0200

    add project component

---
+++ frontend/src/pages/project/Project.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Project = ({
+  title,
+  description,
+  github_repo_link,
+  website_link,
+  main_image,
+  stack,
+  paragraphers,
+  other_links,
+  responsive,
+  images
+}) => {
+  /*const project = {title,
+    description,
+    github_repo_link,
+    website_link,
+    main_image,
+    stack,
+    paragraphers,
+    other_links,
+    responsive,
+    images}*/
+    //console.log(project);
+  return (
+    <article></article>
+  )
+}
+
+export default Project
