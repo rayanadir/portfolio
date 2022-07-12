97dd8a86f363078980b898bcae5316988e32cccf
rayanadir <rayan.dahmena@gmail.com>
2022-07-12 23:34:27 +0200

    prepare languages select

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,7 +1,81 @@
 import React from 'react'
-import '../header/Header.scss'
+import '../header/Header.scss';
+import { styled } from '@mui/material/styles';
+import Switch from '@mui/material/Switch';
+
+// Importations select
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+import french from "../../img/flag/france-flag.png";
+import uk from "../../img/flag/uk-flag.png";
+
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+  width: 62,
+  height: 34,
+  padding: 7,
+  '& .MuiSwitch-switchBase': {
+    margin: 1,
+    padding: 0,
+    transform: 'translateX(6px)',
+    '&.Mui-checked': {
+      color: '#fff',
+      transform: 'translateX(22px)',
+      '& .MuiSwitch-thumb:before': {
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          '#fff',
+        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+      },
+      '& + .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+      },
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
+    width: 32,
+    height: 32,
+    '&:before': {
+      content: "''",
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      left: 0,
+      top: 0,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+        '#fff',
+      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+    },
+  },
+  '& .MuiSwitch-track': {
+    opacity: 1,
+    backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+    borderRadius: 20 / 2,
+  },
+}));
+
 
 const Header = () => {
+
+  const [language, setLanguage] = React.useState("fr");
+
+  React.useEffect(() => {
+    const languageInput=document.querySelector('.css-1d3z3hw-MuiOutlinedInput-notchedOutline');
+    const arrowDropdown=document.querySelector('.css-hfutr2-MuiSvgIcon-root-MuiSelect-icon');
+    languageInput.id="languageInput";
+    arrowDropdown.id="arrowDropdown";
+    const input = document.getElementById('languageInput');
+    const arrow= document.getElementById('arrowDropdown');
+    input.style.borderStyle="none";
+    arrow.style.display="none";
+  }, [])
+  
   return (
     <header className='header'>
       <div className="header__wrapper">
@@ -11,9 +85,28 @@ const Header = () => {
         </div>
         <nav className='header__nav'>
           <ul className='header__nav_list'>
+
             <li className='header__nav_element'>
               Contact
             </li>
+
+              <Box>
+                <FormControl  fullWidth>
+                  {/*<InputLabel>{language === "fr" ? <img className='header__flag' src={french} alt="french flag"/>  : language === "en" ? <img className='header__flag' src={uk} alt="uk flag"/> : ""}</InputLabel>*/}
+                  <Select
+                    value={language}
+                    onChange={(e)=>{setLanguage(e.target.value)}}
+                    sx={{ height: "34px", borderRadius: "0", color:"white", fontWeight:'100' }}
+                    id='language_button'
+                  >
+                    <MenuItem value="fr"><img className='header__flag' src={french} alt="french flag" id="french"/> Français</MenuItem>
+                    <MenuItem value="en"><img className='header__flag' src={uk} alt="uk flag" id="uk" /> Anglais</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+
+            <MaterialUISwitch sx={{ m: 0 }} defaultChecked />
+
             <li className='header__nav_element'>
               À propos
             </li>
