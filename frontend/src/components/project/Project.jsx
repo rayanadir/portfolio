a6a120eb219f42927ac5dbf5616c5c0a797beaa6
rayanadir <rayan.dahmena@gmail.com>
2022-07-12 23:35:11 +0200

    project card responsive

--- frontend/src/components/project/Project.jsx
+++ frontend/src/components/project/Project.jsx
@@ -1,49 +1,30 @@
 import React from 'react';
 import '../project/Project.scss';
 import { STACK } from '../../data/stack';
+import { Link } from "react-router-dom"
 
-const Project = ({
-  title,
-  description,
-  github_repo_link,
-  website_link,
-  main_image,
-  stack,
-  paragraphers,
-  other_links,
-  responsive,
-  images
-}) => {
-  /*const project = {
-    title,
-    description,
-    github_repo_link,
-    website_link,
-    main_image,
-    stack,
-    paragraphers,
-    other_links,
-    responsive,
-    images
-  }
-  console.log(project);*/
+const Project = ({ id, title, description, main_image, stack }) => {
+
   return (
-    <article className="project">
-
+    <article className="project" id={id}>
         <div className="project__image_informations">
-          <img className='project__mainImage' src={main_image} alt={title} />
-          <div className="project__top_info">
-            <h2 className='project__title'>{title}</h2>
-            <p className='project__description'>{description}</p>
-          </div>          
-        </div>
-
-          <div className="project__bottom_info">
+          <Link to={`/project/${id}`} className="project__link">
+            <img className='project__mainImage' src={main_image} alt={title} />
+          </Link>
+          <div className="project__informations">
+            <div className="project__title_desc">
+              <Link to={`/project/${id}`} className='project__link'>
+                <h2 className='project__title'>{title}</h2>
+              </Link>
+              <p className='project__description'>{description}</p>              
+            </div>
+            <div className="project__bottom_info">
             <ul className='project__list'>
             {
               stack.map((element) => {
                 return <li className='project__list__element' key={element}>
                   {
+                    // eslint-disable-next-line array-callback-return
                     STACK.map((s) => {
                       if(s.name===element){
                         return <img src={s.icon} alt={s.id} key={s.id} className='project__list__icon'/>
@@ -53,9 +34,10 @@ const Project = ({
                 </li>
               })
             }
-            </ul>
-            <p className='project__access'>Voir le projet</p>    
+            </ul>   
           </div>
+          </div>          
+        </div>
     </article>
   )
 }
