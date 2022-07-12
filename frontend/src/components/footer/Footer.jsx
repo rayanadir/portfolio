752a58f73c8861c7d474ce22aa6800df2e9fa401
rayanadir <rayan.dahmena@gmail.com>
2022-07-12 23:32:59 +0200

    update footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className='footer' id="footer">
         <p className='footer__element'>©{year}, Rayan Dahmena</p>
-        <p className="footer__element"><a href="https://www.linkedin.com/in/rayan-dahmena-5687b2244/" className='footer__link'>LinkedIn</a></p>
+        <p className="footer__element"><a href="https://www.linkedin.com/in/rayan-dahmena/" className='footer__link'>LinkedIn</a></p>
         <p className='footer__element'><a href="https://github.com/rayanadir" className='footer__link'>Github</a></p>
         <p className='footer__element'>rayan.dahmena@gmail.com</p>
     </footer>
