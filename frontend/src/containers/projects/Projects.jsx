b04c21be78f956cd3e88ac8abf64d8641e701e3d
rayanadir <rayan.dahmena@gmail.com>
2022-07-12 23:36:43 +0200

    prepare projects section

--- frontend/src/containers/projects/Projects.jsx
+++ frontend/src/containers/projects/Projects.jsx
@@ -6,20 +6,16 @@ import { PROJECTS } from '../../data/projects';
 const Projects = () => {
   return (
     <section className="projects" id="projects">
+      <h1 className='projects__title'>Mes projets</h1>
       <div className="projects__list">
         {PROJECTS.map((project) => {
             return <Project 
                       key={project.title}
                       description={project.description}
-                      github_repo_link={project.github_repo_link}
-                      images={project.images}
                       main_image={project.main_image}
-                      other_links={project.other_links}
-                      paragraphers={project.paragraphers}
-                      responsive={project.responsive}
                       stack={project.stack}
                       title={project.title}
-                      website_link={project.website_link}
+                      id={project.id}
                       /> 
           })}
       </div>
