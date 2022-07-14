e73aabf0992873933e307e51d0b2c4c14a126d3c
rayanadir <rayan.dahmena@gmail.com>
2022-07-15 00:45:02 +0200

    add translation

--- frontend/src/containers/aboutme/AboutMe.jsx
+++ frontend/src/containers/aboutme/AboutMe.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import '../aboutme/AboutMe.scss';
 import { STACK } from '../../data/stack';
 import Stack from '../../components/stack/Stack';
+import { useTranslation } from "react-i18next"
 
 const AboutMe = () => {
+
+  const { t } = useTranslation();
 
   const getAge = () => {
     const birthdate = new Date('01-27-2000')
@@ -16,14 +19,14 @@ const AboutMe = () => {
 
   return (
     <section className="aboutme">
-      <h1 className='aboutme__title'>À propos de moi</h1>
-      <h3 className='aboutme__name'>Rayan Dahmena, {getAge()} ans</h3>
-      <p className='aboutme__text'>J'ai suivi la formation de développeur Front-End d'OpenClassrooms. Mon objectif est d'obtenir un poste de développeur Front-End React.js.</p>
-      <p className='aboutme__text'>Ce portfolio est accompagné de plusieurs projets professionnalisants, ainsi que d'autres fonctionnalités.</p>
+      <h1 className='aboutme__title'>{t('aboutme')}</h1>
+      <h3 className='aboutme__name'>{t('age', {age:getAge()})}</h3>
+      <p className='aboutme__text'>{t('formation')}</p>
+      <p className='aboutme__text'>{t('portfolio_projects')}</p>
       <div className="aboutme__stack-container">
-        <Stack key="front" title="Stack Front-End" stackArr={STACK.filter((stack)=> stack.type==="front")} />
-        <Stack key="back" title="Stack Back-End" stackArr={STACK.filter((stack) => stack.type==="back")} />
-        <Stack key="other" title="... et autres" stackArr={STACK.filter((stack) => stack.type==="other")} /> 
+        <Stack key="front" stack="front" title={t('frontend_stack')} stackArr={STACK.filter((stack)=> stack.type==="front")} />
+        <Stack key="back" stack="back" title={t('backend_stack')} stackArr={STACK.filter((stack) => stack.type==="back")} />
+        <Stack key="other" stack="other" title={t('other_stack')} stackArr={STACK.filter((stack) => stack.type==="other")} /> 
       </div>
     </section>
   )
