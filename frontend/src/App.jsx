309574493f9dd9196792fb616c192de181205248
rayanadir <rayan.dahmena@gmail.com>
2022-07-16 00:05:15 +0200

    add projects translation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,22 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home';
 import ProjectPage from './pages/projectPage/ProjectPage';
 import Header from './components/header/Header';
-import Footer from './components/footer/Footer'
+import Footer from './components/footer/Footer';
+import { PROJECTS } from './data/projects';
 import { useTranslation } from "react-i18next";
+import { useDispatch } from 'react-redux';
+import { translateProjects } from './slices/projectsSlice';
 
 const App = ()  => {
   const { t } = useTranslation();
+  const dispatch = useDispatch();
+  React.useEffect(()=> {
+  let projects=PROJECTS.map((project) => {
+    return {...project, description: t(project.id)}
+  })
+  dispatch(translateProjects({projects}))
+  }, [t, dispatch])
+  
   return (
     <React.Fragment>
       <Header />
