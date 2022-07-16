7ea1a4a0326aa11d5f113e3c68b9421145f8a625
rayanadir <rayan.dahmena@gmail.com>
2022-07-17 00:14:02 +0200

    add theme provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.scss';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home';
 import ProjectPage from './pages/projectPage/ProjectPage';
@@ -9,25 +9,29 @@ import { PROJECTS } from './data/projects';
 import { useTranslation } from "react-i18next";
 import { useDispatch } from 'react-redux';
 import { translateProjects } from './slices/projectsSlice';
+import {ThemeProvider} from './context/ThemeContext';
 
-const App = ()  => {
+const App = () => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
-  React.useEffect(()=> {
-  let projects=PROJECTS.map((project) => {
-    return {...project, description: t(project.id)}
-  })
-  dispatch(translateProjects({projects}))
+  useEffect(() => {
+    let projects = PROJECTS.map((project) => {
+      return { ...project, description: t(project.id) }
+    })
+    dispatch(translateProjects({ projects }))
   }, [t, dispatch])
+
   
   return (
     <React.Fragment>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/project/:id' element={<ProjectPage />} />
-      </Routes>
-      <Footer />
+      <ThemeProvider>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/project/:id' element={<ProjectPage />} />
+        </Routes> 
+        <Footer />
+      </ThemeProvider>
     </React.Fragment>
   );
 }
