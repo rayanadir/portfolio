a21506a6202e541ac015f7937e44da7f3e6883ba
rayanadir <rayan.dahmena@gmail.com>
2022-07-17 00:15:56 +0200

    add themes

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import '../footer/Footer.scss'
+import React, { useContext } from 'react';
+import '../footer/Footer.scss';
+import { ThemeContext } from "../../context/ThemeContext";
 
 const Footer = () => {
-  const year = new Date().getFullYear()
+  const year = new Date().getFullYear();
+  // eslint-disable-next-line no-unused-vars
+  const { toggleTheme, theme } = useContext(ThemeContext)
   return (
-    <footer className='footer' id="footer">
+    <footer className={`footer ${theme}`} id="footer">
         <p className='footer__element'>©{year}, Rayan Dahmena</p>
         <p className="footer__element"><a href="https://www.linkedin.com/in/rayan-dahmena/" className='footer__link'>LinkedIn</a></p>
         <p className='footer__element'><a href="https://github.com/rayanadir" className='footer__link'>Github</a></p>
