91ee8eb89fd0ea7d4344629a5b9b54a0420bdcf9
rayanadir <rayan.dahmena@gmail.com>
2022-07-17 00:23:39 +0200

    add theme

--- frontend/src/containers/aboutme/AboutMe.jsx
+++ frontend/src/containers/aboutme/AboutMe.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../aboutme/AboutMe.scss';
 import { STACK } from '../../data/stack';
 import Stack from '../../components/stack/Stack';
-import { useTranslation } from "react-i18next"
+import { useTranslation } from "react-i18next";
+import { ThemeContext } from '../../context/ThemeContext';
 
 const AboutMe = () => {
 
@@ -10,23 +11,28 @@ const AboutMe = () => {
 
   const getAge = () => {
     const birthdate = new Date('01-27-2000')
-    let diff= Math.floor(new Date().getTime() - birthdate.getTime());
+    let diff = Math.floor(new Date().getTime() - birthdate.getTime());
     let day = 1000 * 60 * 60 * 24;
-    let days = Math.floor(diff/day);
-    let years = Math.floor(days/365.25);
+    let days = Math.floor(diff / day);
+    let years = Math.floor(days / 365.25);
     return years;
   }
 
+  // eslint-disable-next-line no-unused-vars
+  const { toggleTheme, theme } = useContext(ThemeContext)
+
   return (
-    <section className="aboutme">
-      <h1 className='aboutme__title'>{t('aboutme')}</h1>
-      <h3 className='aboutme__name'>{t('age', {age:getAge()})}</h3>
-      <p className='aboutme__text'>{t('formation')}</p>
-      <p className='aboutme__text'>{t('portfolio_projects')}</p>
-      <div className="aboutme__stack-container">
-        <Stack key="front" stack="front" title={t('frontend_stack')} stackArr={STACK.filter((stack)=> stack.type==="front")} />
-        <Stack key="back" stack="back" title={t('backend_stack')} stackArr={STACK.filter((stack) => stack.type==="back")} />
-        <Stack key="other" stack="other" title={t('other_stack')} stackArr={STACK.filter((stack) => stack.type==="other")} /> 
+    <section className={`aboutme ${theme}`}>
+      <div className="aboutme__wrapper">
+        <h1 className='aboutme__title'>{t('aboutme')}</h1>
+        <h3 className='aboutme__name'>{t('age', { age: getAge() })}</h3>
+        <p className='aboutme__text'>{t('formation')}</p>
+        <p className='aboutme__text'>{t('portfolio_projects')}</p>
+        <div className="aboutme__stack-container">
+          <Stack key="front" stack="front" title={t('frontend_stack')} stackArr={STACK.filter((stack) => stack.type === "front")} />
+          <Stack key="back" stack="back" title={t('backend_stack')} stackArr={STACK.filter((stack) => stack.type === "back")} />
+          <Stack key="other" stack="other" title={t('other_stack')} stackArr={STACK.filter((stack) => stack.type === "other")} />
+        </div>
       </div>
     </section>
   )
