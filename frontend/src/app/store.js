3bb89aec845fb1715bee3f10b9176e78dffa1ccc
rayanadir <rayan.dahmena@gmail.com>
2022-07-19 18:38:54 +0200

    add languages reducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { projectsReducer } from "../slices/projectsSlice";
+import { languagesReducer } from "../slices/languagesSlice";
 
 export const store = configureStore({
     reducer: {
-        projects: projectsReducer
+        projects: projectsReducer,
+        languages: languagesReducer
     }
 })
