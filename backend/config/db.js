1dc2e7b034e4502c7819a26fba563b8fd56b8e7d
rayanadir <rayan.dahmena@gmail.com>
2022-07-29 21:51:40 +0200

    add database configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,26 @@
-const mongoose = require('mongoose')
-
-mongoose.connect("mongodb+srv://"+ process.env.DB_USER_PASS +"@cluster0.9s38qll.mongodb.net/site-perso")
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.log('Failed to connect to MongoDB', err))
+const { MongoClient } = require("mongodb");
+const Db = process.env.ATLAS_URL;
+const client = new MongoClient(Db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+ 
+var _db;
+ 
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      // Verify we got a good "db" object
+      if (db)
+      {
+        _db = db.db("portfolio_project");
+        console.log("Successfully connected to MongoDB."); 
+      }
+      return callback(err);
+         });
+  },
+ 
+  getDb: function () {
+    return _db;
+  },
+};
