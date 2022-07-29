0406c54496815ec05082162f233a149e51d27c0f
rayanadir <rayan.dahmena@gmail.com>
2022-07-29 21:52:37 +0200

    add controllers functions

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,12 +1,49 @@
-const UserModel = require('../models/user.model')
+const UserModel = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+const { signUpErrors, signInErrors } = require('../utils/errors.utils');
+const dbo = require("../configuration/database");
 
-module.exports.signUp= async (req,res) => {
+
+// Max age of token (3 days)
+const timeLimit = 3*24*60*60*1000;
+
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.TOKEN_SECRET, {
+        expiresIn: timeLimit
+    })
+}
+
+module.exports.signUp= async (req,response) => {
     const {username, email, password} = req.body;
     console.log(req.body);
+    let db_connect = dbo.getDb();
     try{
         const user = await UserModel.create({username, email, password});
         res.status(201).json({user: user._id});
+        db_connect.collection("users").insertOne(user,(err,res) => {
+            if(err) throw err;
+            response.json(res)
+        })
     }catch(err){
-        res.status(200).send({err})
+        const errors = signUpErrors(err)
+        res.status(200).send({errors});
     }
+}
+
+module.exports.signIn= async (req,res) =>{
+    const {email, password} = req.body;
+    try{
+        const user = await UserModel.login(email, password)
+        const token = createToken(user._id);
+        res.cookie('jwt', token, {httpOnly: true, timeLimit});
+        res.status(200).json({user: user._id});
+    }catch(err){
+        const errors = signInErrors(err);
+        res.status(200).json({errors});
+    }
+}
+
+module.exports.logout= (req,res) => {
+    res.cookie('jwt','',{timeLimit:1});
+    res.redirect('/');
 }
---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,13 @@
+const UserModel = require('../models/user.model');
+const ObjectID = require("mongoose").Types.ObjectId;
+
+module.exports.deleteUser= async (req,res) => {
+    if(!ObjectID.isValid(req.params.id))
+        return res.status(400).send("Unknown ID : " + req.params.id);
+    try{
+        await UserModel.remove({_id:req.params.id}).exec();
+        res.status(200).json({message: "Successfully deleted."});
+    }catch(err){
+        return res.status(500).json({message:err});
+    }
+}
