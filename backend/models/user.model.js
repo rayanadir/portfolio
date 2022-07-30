1c5bc0947cfb2a53e6dd0b483d872f16550139a1
rayanadir <rayan.dahmena@gmail.com>
2022-07-31 00:26:09 +0200

    update user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,6 +1,4 @@
 const mongoose= require('mongoose');
-const { isEmail } = require('validator');
-const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username : {
@@ -14,7 +12,6 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        validate: [isEmail],
         lowercase: true,
         unique: true,
         trim: true,
@@ -24,24 +21,20 @@ const userSchema = new mongoose.Schema({
         required: true,
         max: 1024,
         minlength: 6,
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+    },
+    first_login:{
+        type: Date,
+        required:true,
+    },
+    last_login:{
+        type:Date,
+        required:true,
     }
 })
-
-userSchema.pre("save", async (next) => {
-    const salt = await bcrypt.genSalt();
-    this.password= await bcrypt.hash(this.password, salt);
-    next();
-})
-
-userSchema.statics.login = async (email, password) => {
-    const user = await this.findOne({email});
-    if(user){
-        const auth = await bcrypt.compare(password, user.password);
-        if(auth) return user;
-        throw Error('incorrect password');
-    }
-    throw Error('incorrect email');
-}
 
 const UserModel = mongoose.model('user', userSchema);
 
