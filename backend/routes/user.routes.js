a897fb818dc92f9c6c0d396590b8b0fbd6f658df
rayanadir <rayan.dahmena@gmail.com>
2022-07-31 00:27:33 +0200

    correct routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,10 +4,11 @@ const userController = require('../controllers/user.controller.js');
 
 // auth requests
 router.post('/api/register', authController.signUp);
+
 router.post('/api/login', authController.signIn);
 router.post('/api/logout', authController.logout);
 
 // user requests
-router.delete("/api/:id", userController.deleteUser);
+router.delete("/api/delete/:id", userController.deleteUser);
 
 module.exports = router;
