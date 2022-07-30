89107cb31879385e8694039f983d35511a574748
rayanadir <rayan.dahmena@gmail.com>
2022-07-31 00:23:26 +0200

    add auth functions

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,8 @@
 const UserModel = require('../models/user.model');
 const jwt = require('jsonwebtoken');
-const { signUpErrors, signInErrors } = require('../utils/errors.utils');
-const dbo = require("../configuration/database");
-
+const dbo = require("../config/db");
+const ObjectId = require("mongodb").ObjectId;
+const bcrypt = require('bcrypt')
 
 // Max age of token (3 days)
 const timeLimit = 3*24*60*60*1000;
@@ -13,33 +13,76 @@ const createToken = (id) => {
     })
 }
 
+// register
 module.exports.signUp= async (req,response) => {
-    const {username, email, password} = req.body;
-    console.log(req.body);
     let db_connect = dbo.getDb();
     try{
-        const user = await UserModel.create({username, email, password});
-        res.status(201).json({user: user._id});
-        db_connect.collection("users").insertOne(user,(err,res) => {
-            if(err) throw err;
-            response.json(res)
+        const {username, email, password, confirmPassword} = req.body;
+        
+        if(!email || !username || !password || !confirmPassword){
+            return response.status(400).json({message:"One or several fields are missing.", msg_code:"field_miss"});
+        }
+        if(password.length<6){
+            return response.status(400).json({message: "Password length must be at least 6 characters", msg_code:"password_length"});
+        }
+        if(password !== confirmPassword){
+            return response.status(400).json({message:"The passwords are not identical",msg_code:"different_passwords"});
+        }
+        const isUserExists= await UserModel.findOne({email});
+        if(isUserExists){
+            return response.status(400).json({message:"An account already uses this email",msg_code:"account_exists"})
+        }
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash= await bcrypt.hash(password,salt);
+
+        const newUser = new UserModel({
+            email,
+            username,
+            password:passwordHash,
+            first_login: new Date(),
+            last_login: new Date(),
+            isAdmin:false
+        });
+        db_connect.collection("users").insertOne(newUser,(err,res) => {
+            if(err) {throw err;}
+            response.status(201).json({res,user: user._id});
         })
     }catch(err){
-        const errors = signUpErrors(err)
-        res.status(200).send({errors});
+        response.status(500).json({error:"Server error"});
     }
 }
 
-module.exports.signIn= async (req,res) =>{
-    const {email, password} = req.body;
+// login
+module.exports.signIn= async (req,response) =>{
+    let db_connect = dbo.getDb();
     try{
-        const user = await UserModel.login(email, password)
-        const token = createToken(user._id);
-        res.cookie('jwt', token, {httpOnly: true, timeLimit});
-        res.status(200).json({user: user._id});
+        const {email, password} = req.body;
+        if(!email || !password){
+            return response.status(400).json({message:"One or several fields are missing.",msg_code:"field_miss"})
+        }
+        const user = await UserModel.findOne({email});
+        if(!user){
+            return response.status(400).json({message:"Unknown email address",msg_code:"unknown_email"})
+        }
+        const correctPassword = await bcrypt.compare(password, user.password);
+        if(!correctPassword){
+            return response.status(400).json({message:"Incorrect password",msg_code:"incorrect_password"})
+        }
+        let token = createToken(user._id) 
+        const update = {
+            last_login: new Date(),
+        }
+        db_connect.collection('users').updateOne(user._id,update,(err,res)=> {
+            if(err) {throw err;}
+            response.status(201).json({token,res,user:{
+                id:user._id,
+                username:user.username,
+                email:user.email,
+            }})
+        })
     }catch(err){
-        const errors = signInErrors(err);
-        res.status(200).json({errors});
+        response.status(500).json({error:"Server error"})
     }
 }
 
