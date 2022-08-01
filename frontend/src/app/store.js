1463c2ea6a49718a05f1995b4f95761d410a95c1
rayanadir <rayan.dahmena@gmail.com>
2022-08-02 00:39:56 +0200

    add middleware

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { projectsReducer } from "../slices/projectsSlice";
 import { languagesReducer } from "../slices/languagesSlice";
+import { authReducer } from "../slices/authSlice";
 
 export const store = configureStore({
     reducer: {
         projects: projectsReducer,
-        languages: languagesReducer
-    }
+        languages: languagesReducer,
+        auth: authReducer,
+    },
+    middleware: (getDefaultMiddleWare) => getDefaultMiddleWare({serializableCheck:false})
 })
