94c5ea8daf7825e46ee33c01c85b44ef088b7c9f
rayanadir <rayan.dahmena@gmail.com>
2022-08-01 20:19:17 +0200

    change database server

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,25 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
-app.use(cors());
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+
+// get driver connection
+//const dbo = require("./config/db");
+require("./config/db");
 app.use(express.json());
 app.use(require("./routes/user.routes"))
-// get driver connection
-const dbo = require("./config/db");
  
 app.listen(port, () => {
   // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
+  /*dbo.connectToServer(function (err) {
     if (err) console.error(err);
  
-  });
+  });*/
   console.log(`Server is running on port: ${port}`);
 });
