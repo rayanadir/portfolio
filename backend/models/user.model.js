4611e0162b3d43b97f8f915e6c9bcdaff3bdfbd5
rayanadir <rayan.dahmena@gmail.com>
2022-08-01 20:24:21 +0200

    change user schema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,41 +1,15 @@
-const mongoose= require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username : {
-        type: String,
-        required: true,
-        minLength: 3,
-        maxLength: 55,
-        unique: true,
-        trim: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        lowercase: true,
-        unique: true,
-        trim: true,
-    },
-    password: {
-        type: String,
-        required: true,
-        max: 1024,
-        minlength: 6,
-    },
-    isAdmin: {
-        type: Boolean,
-        required: true,
-    },
-    first_login:{
-        type: Date,
-        required:true,
-    },
-    last_login:{
-        type:Date,
-        required:true,
-    }
-})
+  email: { type: String, required: true },
+  username:{type:String, required:true},
+  passwordHash: { type: String, required: true },
+  isAdmin: { type:Boolean, required:true },
+  first_login : {type: Date, required: true},
+  last_login : { type: Date, required:true}
+}
+);
 
-const UserModel = mongoose.model('user', userSchema);
+const User = mongoose.model("user", userSchema);
 
-module.exports = UserModel
+module.exports = User;
