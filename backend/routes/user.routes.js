b094080d63e5fa023bdc95bc7f901328219ce191
rayanadir <rayan.dahmena@gmail.com>
2022-08-03 23:48:06 +0200

    add password routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller.js");
 const userController = require('../controllers/user.controller.js');
+const auth = require("../middleware/auth.middleware")
 
 // register
 router.post("/api/register", authController.signUp);
@@ -14,5 +15,13 @@ router.get("/api/logout", authController.logout);
 // loggedIn
 router.get("/api/loggedIn", authController.loggedIn);
 
+// resetPassword
+router.put("/api/resetPassword/:token", authController.resetPassword);
+
+// forgotPassword
+router.put("/api/forgotPassword", authController.forgotPassword);
+
+// changePassword
+router.put("/api/changePassword", auth ,authController.changePassword);
 
 module.exports = router;
