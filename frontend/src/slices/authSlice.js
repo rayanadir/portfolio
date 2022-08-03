fb02474d017b431c12162f9f1c3bedf585c79f6e
rayanadir <rayan.dahmena@gmail.com>
2022-08-03 23:53:46 +0200

    add password states slice

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -10,13 +10,26 @@ const authState= {
         message:'',
         code_msg:'',
     },
+    reset_password:{
+        message:'',
+        code_msg:'',
+    },
+    forgot_password:{
+        message:'',
+        code_msg:'',
+        status:'inital'
+    },
+    change_password:{
+        message:'',
+        code_msg:'',
+    }
 }
 
 const authSlice = createSlice({
     name:"authSlice",
     initialState:authState,
     reducers: {
-        loginSuccess: (state,action) => {
+        loginSuccess: (state) => {
             state.login_error={
                 message:'',
                 code_msg:'',
@@ -25,7 +38,7 @@ const authSlice = createSlice({
         loginFail: (state,action) => {
             state.login_error=action.payload.data;
         },
-        registerSuccess: (state,action) => {
+        registerSuccess: (state) => {
             state.register_error={
                 message:'',
                 code_msg:'',
@@ -33,10 +46,19 @@ const authSlice = createSlice({
         },
         registerFail: (state,action) => {
             state.register_error=action.payload.data;
+        },
+        resetPasswordRes: (state,action) => {
+            state.reset_password=action.payload;
+        },
+        forgotPasswordRes: (state,action) => {
+            state.forgot_password=action.payload;
+        },
+        changePasswordRes: (state,action) => {
+            state.change_password=action.payload.data;
         }
     }
 })
 
-export const { loginSuccess, loginFail, registerSuccess, registerFail } = authSlice.actions;
+export const { loginSuccess, loginFail, registerSuccess, registerFail, resetPasswordRes, forgotPasswordRes,changePasswordRes } = authSlice.actions;
 
 export const authReducer = authSlice.reducer;
