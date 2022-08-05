4a2ee1f5bb084076c0f91710485511854f697194
rayanadir <rayan.dahmena@gmail.com>
2022-08-05 20:52:47 +0200

    send mail function

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,23 +1,43 @@
 const nodemailer = require('nodemailer');
 
-let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-        user: "devitproject.media@gmail.com", // generated ethereal user
-        pass: "ezwvvpfzwrljqzbf"  // generated ethereal password
+module.exports.sendEmail = async (email,username,subject,url) => {
+    try{
+        const transporter = nodemailer.createTransport({
+            host:process.env.HOST,
+            service:process.env.SERVICE,
+            port:Number(process.env.EMAIL_PORT),
+            secure: Boolean(process.env.SECURE),
+            auth:{
+                user:process.env.USER,
+                pass:process.env.PASS
+            },
+            tls:{
+                rejectUnauthorized:false,
+            }
+        })
+        await transporter.sendMail({
+            from:process.env.USER,
+            to:email,
+            subject,
+            text:"test",
+            html:
+            `
+            <div>
+                <h1>Bonjour, Hello ${username} !</h1>
+                <br>
+                <h2>FR</h2>
+                <p>Vous avez demandé à réinitialiser votre mot de passe, veuillez cliquer sur le lien ci-dessous. Si vous n'êtes pas à l'origine de cette action, ignorez ce mail.</p>
+                <br>
+                <h2>EN</h2>
+                <p>You have requested to reset your password, please click on the link below. If you did not initiate this action, ignore this email.</p>
+
+                <a href=${url}>${url}</a>
+            </div>
+            `,
+        });
+        console.log("email sent successfully");
+    }catch(err){
+        console.log("Email not sent");
+        console.log(err)
     }
-});
-
-let sendEmail = (emailTemplate) => {
-    transporter.sendMail(emailTemplate, (err, info) => {
-        if(err) {
-            console.log(err)
-        }else{
-            console.log('Email sent: ', info.response)
-        }
-    })
 }
-
-module.exports = {sendEmail};
