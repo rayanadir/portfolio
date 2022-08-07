e44553fb1f10a56697044c62a91c549cdac938ed
rayanadir <rayan.dahmena@gmail.com>
2022-08-07 23:31:11 +0200

    add user reducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,12 +2,14 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { projectsReducer } from "../slices/projectsSlice";
 import { languagesReducer } from "../slices/languagesSlice";
 import { authReducer } from "../slices/authSlice";
+import { userReducer } from "../slices/userSlice";
 
 export const store = configureStore({
     reducer: {
         projects: projectsReducer,
         languages: languagesReducer,
         auth: authReducer,
+        user:userReducer
     },
     middleware: (getDefaultMiddleWare) => getDefaultMiddleWare({serializableCheck:false})
 })
