6f78e397af00ccf68e9bba445e0bff59b1532f15
rayanadir <rayan.dahmena@gmail.com>
2022-08-12 23:07:26 +0200

    add conversation services functions

--- frontend/src/services/conversation.service.js
+++ frontend/src/services/conversation.service.js
@@ -1,9 +1,9 @@
-import { TextareaAutosize } from "@mui/material";
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000";
 
 const newConversation = (userId) => {
+    console.log(userId)
     axios.post(BASE_URL+"/api/newConversation", {userId})
     .then((res) => {
         console.log(res);
@@ -27,6 +27,30 @@ const sendMessage = (message, userId, id) => {
     })
 }
 
-const conversation_service = { newConversation, sendMessage }
+const getConversation = (id, userId) => {
+    axios.post(BASE_URL+"/api/conversation", {id, userId})
+    .then((res) => {
+        console.log(res);
+        return res;
+    })
+    .catch((err) => {
+        console.log(err);
+        return err;
+    }) 
+}
+
+const getConversations = () => {
+    axios.post(BASE_URL+"/api/conversations")
+    .then((res) => {
+        console.log(res);
+        return res;
+    })
+    .catch((err) => {
+        console.log(err);
+        return err;
+    }) 
+}
+
+const conversation_service = { newConversation, sendMessage, getConversation, getConversations }
 
 export default conversation_service;
