c407a9d0fa89d970a225dd7489ba0dd3367eec79
rayanadir <rayan.dahmena@gmail.com>
2022-08-12 23:00:40 +0200

    add routes

--- backend/routes/conversation.routes.js
+++ backend/routes/conversation.routes.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
 const conversationController = require('../controllers/conversation.controller');
+const conversation = require("../middleware/conversation.middleware")
 
 // start new conversation
-router.post("/api/newConversation", conversationController.newConversation);
+router.post("/api/newConversation", conversation,conversationController.newConversation);
 
 // send message
-router.post("/api/sendMessage", conversationController.sendMessage)
+router.post("/api/sendMessage", conversation,conversationController.sendMessage);
+
+// get single conversation
+router.post("/api/conversation/:id", conversation,conversationController.getSingleConversation);
+
+// get all conversations
+router.post("/api/conversations", conversation,conversationController.getAllConversations);
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller.js");
 const userController = require('../controllers/user.controller.js');
+const conversationController = require('../controllers/conversation.controller')
 const auth = require("../middleware/auth.middleware")
 const resetMiddleware = require("../middleware/reset.middleware")
+const conversation = require('../middleware/conversation.middleware')
 
 // register
 router.post("/api/register", authController.signUp);
@@ -30,5 +32,19 @@ router.post("/api/getUser", auth,userController.getUser);
 
 // checkTokenValidity
 router.post("/api/checkToken", resetMiddleware,authController.checkToken)
+
+// start new conversation
+router.post("/api/newConversation", conversation,conversationController.newConversation);
+
+// send message
+router.post("/api/sendMessage", conversation,conversationController.sendMessage);
+
+// get single conversation
+router.post("/api/conversation/:id", conversation,conversationController.getSingleConversation);
+
+// get all conversations
+router.post("/api/conversations", conversation,conversationController.getAllConversations);
+
+
 
 module.exports = router;
