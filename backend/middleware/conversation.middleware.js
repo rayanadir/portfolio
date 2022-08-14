1ecb7316dd7669adf282d6f2d30ca66440514d0b
rayanadir <rayan.dahmena@gmail.com>
2022-08-14 23:49:46 +0200

    change token request

--- backend/middleware/conversation.middleware.js
+++ backend/middleware/conversation.middleware.js
@@ -1,11 +1,14 @@
 const jwt = require("jsonwebtoken");
 
 function conversation(req,res,next){
+    //console.log(req)
     try{
-        const token = req.cookies.token;
+        const token = req.body.token;
+        //console.log("token : "+token)
         if (!token) return res.status(401).json({ errorMessage: "Unauthorized" });
         const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-        req.userId= verified.userId
+        req.userId= verified.userId;
+        //console.log("verified user : "+verified.userId)
         next();
     }catch(err){
         console.error(err);
