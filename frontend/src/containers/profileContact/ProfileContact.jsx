b305f2c6295369ed20fb9bd5bf0ef654fa065f42
rayanadir <rayan.dahmena@gmail.com>
2022-08-14 23:52:51 +0200

    check user and display conversations list

--- frontend/src/containers/profileContact/ProfileContact.jsx
+++ frontend/src/containers/profileContact/ProfileContact.jsx
@@ -6,55 +6,80 @@ import { useSelector } from "react-redux";
 import { useNavigate } from 'react-router-dom';
 import conversation_service from '../../services/conversation.service';
 import axios from "axios";
+import moment from 'moment/min/moment-with-locales';
 
 
 const ProfileContact = ({ user }) => {
     const { t } = useTranslation();
     const navigate = useNavigate();
     const token = useSelector((state) => state.auth.token !== null ? state.auth.token : localStorage.getItem('token') !== null ? localStorage.getItem('token') : null);
+    const conversations = useSelector((state) => state.user.conversationsData);
+    const conversation = useSelector((state) =>  state.user.conversationData);
     useEffect(() => {
-        //conversation_service.getConversations()
-        axios.post("http://localhost:5000/api/conversations", { token }, {
-            headers: { "Authorization": `Bearer ${token}` }
-        })
-            .then((res) => {
-                console.log(res);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    },[token])
+        if (user.isAdmin) {
+            conversation_service.getConversations(user.userId)
+        } else {
+            conversation_service.checkHasConversation(user.userId)
+        }
+
+    }, [token, user.isAdmin, user.userId])
+    const formatDate = (date) => {
+        moment.locale(localStorage.getItem('lang'))
+        return moment(date).format('LLLL')
+    }
+
     return (
         <article className='profileContact'>
             {
-                user.isAdmin === false ?
+                user.isAdmin === true ?
                     <section>
-                        {/*<p className='profileContact__noMessages'>Aucun message pour le moment</p>*/}
-                            <h2 className='profileContact__title'>Derniers messages</h2>
-                            <ul className='profileContact__list'>
-                                <li className='profileContact__list__element' onClick={() => { navigate('/conversation/1') }}>
-                                    <div className='profileContact__list__element__name-time'>
-                                        <p className='profileContact__list__element__name-time__name'>Nom Prénom</p>
-                                        <p className='profileContact__list__element__name-time__time'>Date</p>
-                                    </div>
-                                    <p className='profileContact__list__element__text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                                </li>
-                            </ul>
-                    </section>
-                    :
-                    <section>
-                        <h2 className='profileContact__lastMessages'>Démarrez une conversation</h2>
-                        <Button id="new_conversation" onClick={() => { conversation_service.newConversation(user.userId) }} style={{ textTransform: "none" }}>Lancer conversation</Button>
+                        {
+                            conversations.conversations && conversations.conversations.length > 0 ?
+                                <>
+                                    <h2 className='profileContact__title'>Derniers messages</h2>
+                                    <ul className='profileContact__list'>
+                                        {
+                                            conversations.conversations.map((conversation) => {
+                                                return <li key={conversation.id} className='profileContact__list__element' onClick={() => { navigate(`/conversation/${conversation.id}`) }}>
+                                                    <div className='profileContact__list__element__name-time'>
+                                                        <p className='profileContact__list__element__name-time__name'>{conversation.username}</p>
+                                                        <p className='profileContact__list__element__name-time__time'>{formatDate(conversation.updatedAt)}</p>
+                                                    </div>
+                                                    <p className='profileContact__list__element__text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                                </li>
+                                            })
+                                        }
 
-                        <h2 className='profileContact__title'>Suivre ma conversation</h2>
-                        <li className='profileContact__list__element' onClick={() => { navigate('/conversation/1') }}>
-                            <div className='profileContact__list__element__name-time'>
-                                <p className='profileContact__list__element__name-time__name'>Nom Prénom</p>
-                                <p className='profileContact__list__element__name-time__time'>Date</p>
-                            </div>
-                            <p className='profileContact__list__element__text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        </li>
+                                    </ul>
+                                </>
+                                : conversations.conversations && conversations.conversations.length === 0 ?
+                                    <p className='profileContact__noMessages'>Aucun message pour le moment</p>
+                                    : null
+                        }
                     </section>
+                    : user.isAdmin === false ?
+                    <section>
+                        {
+                            conversation && conversation.code_msg === "no_conversation_started" ?
+                                <>
+                                    <h2 className='profileContact__title'>Démarrez une conversation</h2>
+                                    <Button id="new_conversation" onClick={() => { navigate('/conversation/new');/*conversation_service.newConversation(user.userId)*/ }} style={{ textTransform: "none" }}>Lancer conversation</Button>
+                                </>
+                                : conversation && conversation.code_msg === "conversation_already_started" ?
+                                    <>
+                                        <h2 className='profileContact__title'>Suivre ma conversation</h2>
+                                        <li className='profileContact__list__element' onClick={() => { navigate(`/conversation/${conversation.conversation.id}`) }}>
+                                            <div className='profileContact__list__element__name-time'>
+                                                <p className='profileContact__list__element__name-time__name'>{conversation.conversation.username}</p>
+                                                <p className='profileContact__list__element__name-time__time'>{formatDate(conversation.updatedAt)}</p>
+                                            </div>
+                                            <p className='profileContact__list__element__text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                        </li>
+                                    </>
+                                : null
+                        }
+                    </section>
+                    : null
             }
         </article>
     )
