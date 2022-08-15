4d5ea0283acefd80c617a1897f7616ed15f0b890
rayanadir <rayan.dahmena@gmail.com>
2022-08-15 23:09:39 +0200

    position + color of message

--- frontend/src/components/message/Message.jsx
+++ frontend/src/components/message/Message.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import '../message/Message.scss'
+import React, { useContext } from 'react';
+import '../message/Message.scss';
+import { ThemeContext } from '../../context/ThemeContext';
 
-const Message = ({message, timestamp, isAuthor}) => {
+const Message = ({ message, timestamp, isAuthor }) => {
+  // eslint-disable-next-line no-unused-vars
+  const { toggleTheme, theme } = useContext(ThemeContext);
+  const bubbleColor = isAuthor && theme==="dark" ? "deepskyblue" 
+                    : isAuthor && theme==="light" ? "#3583DD"
+                    : isAuthor === false && theme === "dark" ? "grey"
+                    : isAuthor === false && theme === "light" ? "#606060"
+                    : null;
+  const color = theme === "dark" ? "black" : theme === "light" ? "white" : "null"
+  
   return (
-    <article className="message" style={{alignItems: isAuthor ? "flex-end" : "flex-start"}}>
-        <p className='message__bubble' style={{backgroundColor: isAuthor ? "deepskyblue" : "grey"}}>{message}</p>
+    <div style={{width:"100%"}}>
+      <article className="message" style={{ alignItems: isAuthor ? "flex-end" : "flex-start" }}>
+        <p className='message__bubble' style={{ backgroundColor: bubbleColor, color }}>{message}</p>
         <span className='message__timestamp'>{timestamp}</span>
-    </article>
+      </article>
+    </div>
+
   )
 }
 
