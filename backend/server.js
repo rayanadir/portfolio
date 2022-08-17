17e2143795faddc06585b8947878c88a47a7d427
rayanadir <rayan.dahmena@gmail.com>
2022-08-17 23:50:27 +0200

    add socket query

--- backend/server.js
+++ backend/server.js
@@ -16,29 +16,57 @@ const mongoose = require('mongoose');
 
 const io = require('socket.io')(8900,{
   cors:{
-      origin: "http://localhost:3000"
-  }
+      origin: "http://localhost:3000",
+  },
 })
 
-io.on("connection",  (socket) => {
+io.on("connection", (socket) => {
 
-  const userId = socket.handshake.query.userId
-  // get all conversations
-  Conversation.find({}).sort({ updatedAt: -1 }).lean().exec(async (err, doc) => {
-    if (err) {
-      socket.emit('getConversations', [])
-    } else if (doc) {
-      const conversations = doc.map(async (conversation) => {
-        const otherUser = conversation.users.find(u => u !== userId)
-        const user = await User.findOne({ userId: otherUser })
-        conversation.username = user.username
-        return conversation;
-      })
-      Promise.all(conversations).then((values) => {
-        socket.emit("getConversations", values)
-      })
+  const userId = socket.handshake.query.userId;
+  User.findOne({userId}).lean().exec(async (err,user) => {
+    if(err){
+      return null;
+    }else if(user){
+      if(user.isAdmin===true){
+        // get all conversations
+        Conversation.find({}).sort({ updatedAt: -1 }).lean().exec(async (err, doc) => {
+          if (err) {
+            socket.emit('getConversations', [])
+          } else if (doc) {
+            const conversations = doc.map(async (conversation) => {
+              const otherUser = conversation.users.find(u => u !== userId)
+              const user = await User.findOne({ userId: otherUser })
+              conversation.username = user.username
+              return conversation;
+            })
+            Promise.all(conversations).then((values) => {
+              socket.emit("getConversations", values)
+            })
+          }
+        })
+      }else if (user.isAdmin === false) {
+        // get single conversation 
+          try{
+            Conversation.findOne({users:userId}).lean().exec( async(err,doc) => {
+              if(err){
+                socket.emit('getSingleConversation',{code_msg:"error"})
+              }
+              else if(!doc){
+                socket.emit('getSingleConversation', {code_msg:"no_conversation_started"})
+              }
+              else if(doc){
+                const otherUser = doc.users.find(u => u!==userId)
+                const user = await User.findOne({userId: otherUser});
+                doc.username = user.username;
+                socket.emit('getSingleConversation', doc)
+              }
+            })
+          }catch(err){
+            socket.emit('getSingleConversation',{code_msg:"error"})
+          }
+      }
     }
-  })
+  });
 
   // get all messages from a conversation
   const conversationId = socket.handshake.query.conversationId
@@ -53,31 +81,44 @@ io.on("connection",  (socket) => {
   }
 
 
+
   // send a message
   socket.on('newMessage', async (data) => {
-    console.log(data)
     try {
       const conversationId =socket.handshake.query.conversationId
       if(conversationId !== undefined){
-        const conversation = await Conversation.findOne({id: conversationId})
+        const existingConversation = await Conversation.findOne({id: conversationId})
         const messageId = new mongoose.Types.ObjectId().toHexString()
-        const messageObj = { message: data.message, userId: data.userId, date: new Date().toISOString(), id: messageId }
-        await conversation.updateOne({
-          $push: {messages: messageObj}
-        })
-        io.emit('newMessage', { message: data.message, userId: data.userId, date: new Date().toISOString(), id: messageId })
+        const messageObj = { message: data.message, userId: data.userId, date: data.date, id: messageId }
+        const otherUser = existingConversation.users.filter(u=> u!==userId);
+        const user = await User.findOne({userId: otherUser});
+        const username = user.username;
+        const conversation = await Conversation.findOneAndUpdate({id: conversationId},
+          {
+            $push: {messages: messageObj},
+          },
+          {
+            new:true
+          });
+          //conversation.username= username
+          const updatedConversation = {...conversation, username}
+          io.emit('newMessage', { message: data.message, userId: data.userId, date: data.date, id: messageId });
+          io.emit('updateConversation', conversation);
+          io.emit('getSingleConversation', updatedConversation);
       }else if(conversationId === undefined){
         const adminUser = await User.findOne({ email: String(process.env.USER) });
         const newConversationId = new mongoose.Types.ObjectId().toHexString();
         const messageId=new mongoose.Types.ObjectId().toHexString();
-        const newMessage =[{message:data.message, userId:data.userId, date: new Date().toISOString(), id: messageId}]
+        const newMessage =[{message:data.message, userId:data.userId, date: data.date, id: messageId}]
         const conversation = new Conversation({
             id: newConversationId,
             users:[userId,adminUser.userId],
             messages: newMessage
         });
-        await conversation.save();
-        io.emit('newMessage', { message: data.message, userId: data.userId, date: new Date().toISOString(), id: messageId })
+        await conversation.save()
+        io.emit('newMessage', { message: data.message, userId: data.userId, date: data.date, id: messageId });
+        io.emit('updateConversation', conversation);
+        io.emit('getSingleConversation', { message: data.message, userId: data.userId, date: data.date, id: messageId, username:adminUser.username });
       }
     } catch (error) {
       console.log(error)
