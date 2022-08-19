5568e7b88f8c40fca2a06f7f49c3f106be27cc18
rayanadir <rayan.dahmena@gmail.com>
2022-08-19 18:18:57 +0200

    new conversation function

--- frontend/src/containers/profileContact/ProfileContact.jsx
+++ frontend/src/containers/profileContact/ProfileContact.jsx
@@ -8,6 +8,7 @@ import conversation_service from '../../services/conversation.service';
 import moment from 'moment/min/moment-with-locales';
 import useConversations from '../../pages/conversation/useConversations';
 import useSingleConversation from '../../pages/conversation/useSingleConversation';
+import axios from "axios";
 
 
 const ProfileContact = ({ user }) => {
@@ -16,14 +17,10 @@ const ProfileContact = ({ user }) => {
     const token = useSelector((state) => state.auth.token !== null ? state.auth.token : localStorage.getItem('token') !== null ? localStorage.getItem('token') : null);
     const {conversations} = useConversations();
     const {conversation} = useSingleConversation();
- 
+
     useEffect(() => {
         if (user.isAdmin) {
-            conversation_service.getConversations(user.userId)
-            
-        } else {
-            conversation_service.checkHasConversation(user.userId)
-            
+            conversation_service.getConversations(user.userId)   
         }
     }, [token, user.isAdmin, user.userId])
 
@@ -43,10 +40,11 @@ const ProfileContact = ({ user }) => {
                                     <h2 className='profileContact__title'>{t('last_messages')}</h2>
                                     <ul className='profileContact__list'>
                                         {
-                                            conversations.map((conversation) => {
-                                                return <li key={conversation.id} className='profileContact__list__element' onClick={() => { navigate(`/conversation/${conversation.id}`) }}>
+                                            conversations.map((conversation,i) => {
+                                                const otherUser = conversation.users_arr.find(u => u.userId!==user.userId);
+                                                return <li key={i} className='profileContact__list__element' onClick={() => { navigate(`/conversation/${conversation.id}`) }}>
                                                     <div className='profileContact__list__element__name-time'>
-                                                        <p className='profileContact__list__element__name-time__name'>{conversation.username}</p>
+                                                        <p className='profileContact__list__element__name-time__name'>{otherUser.username}</p>
                                                         <p className='profileContact__list__element__name-time__time'>{formatDate(conversation.messages[conversation.messages.length-1].date)}</p>
                                                     </div>
                                                     <p className='profileContact__list__element__text'>{conversation.messages[conversation.messages.length-1].userId === user.userId ? "Vous : " : ""}{conversation.messages[conversation.messages.length-1].message}</p>
@@ -62,7 +60,7 @@ const ProfileContact = ({ user }) => {
                             <>
                             </>
                             :null
-                        }
+                                    }
                     </section>
                     : user.isAdmin === false ?
                     <section>
@@ -70,14 +68,26 @@ const ProfileContact = ({ user }) => {
                             conversation && conversation.code_msg === "no_conversation_started" ?
                                 <>
                                     <h2 className='profileContact__title'>{t('start_new_conversation')}</h2>
-                                    <Button id="new_conversation" onClick={() => { navigate('/conversation/new') }} style={{ textTransform: "none" }}>{t('new_conversation')}</Button>
+                                    <Button id="new_conversation" onClick={() => {
+                                            axios.post("http://localhost:5000/api/newConversation")
+                                            .then((res) => {
+                                                if(res.data.id){
+                                                    navigate(`/conversation/${res.data.id}`)
+                                                }
+                                                return res;
+                                            })
+                                            .catch((err) => {
+                                                console.log(err);
+                                                return err;
+                                            })
+                                    }} style={{ textTransform: "none" }}>{t('new_conversation')}</Button>
                                 </>
                                 : conversation && conversation.messages.length > 0 ?
                                     <>
                                         <h2 className='profileContact__title'>{t('follow_conversation')}</h2>
                                         <li className='profileContact__list__element' onClick={() => { navigate(`/conversation/${conversation.id}`) }}>
                                             <div className='profileContact__list__element__name-time'>
-                                                <p className='profileContact__list__element__name-time__name'>{conversation.username}</p>
+                                                <p className='profileContact__list__element__name-time__name'>{conversation.users_arr.find(u => u.userId!==user.userId).username}</p>
                                                 <p className='profileContact__list__element__name-time__time'>{formatDate(conversation.messages[conversation.messages.length-1].date)}</p>
                                             </div>
                                             <p className='profileContact__list__element__text'>
