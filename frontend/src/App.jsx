f47046fe2e9e390913e940b112984146bc8eabfa
rayanadir <88830520+rayanadir@users.noreply.github.com>
2022-09-05 19:45:51 +0200

    add paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home';
 import ProjectPage from './pages/projectPage/ProjectPage';
+import Contact from './pages/contact/Contact'
 import Auth from './pages/auth/Auth';
 import Conversation from './pages/conversation/Conversation'
 import Profile from './pages/profile/Profile';
@@ -17,6 +18,8 @@ import { translateProjects } from './slices/projectsSlice';
 import { ThemeProvider } from './context/ThemeContext';
 import Reset from './pages/reset/Reset';
 import Settings from './pages/settings/Settings';
+import MessagePage from './pages/messagePage/MessagePage';
+import SimpleMessage from './pages/simpleMessage/SimpleMessage';
 
 const App = () => {
   const { t } = useTranslation();
@@ -35,6 +38,9 @@ const App = () => {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/project/:id' element={<ProjectPage />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/message" element={<MessagePage />} />
+            <Route path="/message/:id" element={<SimpleMessage />} />
             <Route path='/authentication' element={<Auth />} />
             <Route path='/reset/:token' element={<Reset />} />
             <Route path='/profile' element={<Profile />} />
