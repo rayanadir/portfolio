28f4351457cb40ac6c81924b0134cc0e16177786
rayanadir <rayan.dahmena@gmail.com>
2022-09-05 19:51:00 +0200

    add message reducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -3,13 +3,15 @@ import { projectsReducer } from "../slices/projectsSlice";
 import { languagesReducer } from "../slices/languagesSlice";
 import { authReducer } from "../slices/authSlice";
 import { userReducer } from "../slices/userSlice";
+import { messageReducer } from "../slices/messageSlice";
 
 export const store = configureStore({
     reducer: {
         projects: projectsReducer,
         languages: languagesReducer,
         auth: authReducer,
-        user:userReducer
+        user:userReducer,
+        message: messageReducer
     },
     middleware: (getDefaultMiddleWare) => getDefaultMiddleWare({serializableCheck:false})
 })
