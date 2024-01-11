7f765f7b4e05add748bf231cbe8164fb9cb95eb0
rayanadir <rayan.dahmena@gmail.com>
2024-01-11 22:35:25 +0100

    add projects

--- frontend/src/data/projects.js
+++ frontend/src/data/projects.js
@@ -1,3 +1,58 @@
+import banner_p13_fs from "../img/fullstack/p13/banner_p13.png";
+import diagramme_composants_p13_fs from "../img/fullstack/p13/diagramme_composants.png";
+import diagramme_deploiement_p13_fs from "../img/fullstack/p13/diagramme_deploiement.png";
+import diagramme_modele_de_donnees_p13_fs from "../img/fullstack/p13/diagramme_modele_de_donnees.png";
+
+import banner_p12_fs from "../img/fullstack/p12/banner_p12_fs.png";
+import banner_p11_fs from "../img/fullstack/p11/banner_p11_fs.jpg";
+import banner_p10_fs from "../img/fullstack/p10/banner_p10_fs.png";
+import banner_p9_fs from "../img/fullstack/p9/banner_p9_fs.png";
+
+import banner_p8_fs from "../img/fullstack/p8/banner_p8_fs.png";
+import audit_architecture_p8_fs from "../img/fullstack/p8/audit/architecture.png";
+import audit_clients_p8_fs from "../img/fullstack/p8/audit/clients.png";
+import audit_deliveries_p8_fs from "../img/fullstack/p8/audit/deliveries.png";
+import audit_login_p8_fs from "../img/fullstack/p8/audit/login.png";
+import audit_logout_p8_fs from "../img/fullstack/p8/audit/logout.png";
+import architecture_p8_fs from "../img/fullstack/p8/cahier_des_charges/architecture.png";
+import authentification_deconnexion_p8_fs from "../img/fullstack/p8/cahier_des_charges/authentification_deconnexion.png";
+import client_diagramme_p8_fs from "../img/fullstack/p8/cahier_des_charges/client_diagramme.png";
+import client_p8_fs from "../img/fullstack/p8/cahier_des_charges/client.png";
+import commercial_p8_fs from "../img/fullstack/p8/cahier_des_charges/commercial.png";
+import livraisons_p8_fs from "../img/fullstack/p8/cahier_des_charges/livraisons.png";
+import login_p8_fs from "../img/fullstack/p8/cahier_des_charges/login.png";
+import register_p8_fs from "../img/fullstack/p8/cahier_des_charges/register.png";
+import service_commercial_diagramme_p8_fs from "../img/fullstack/p8/cahier_des_charges/service_commercial_diagramme.png";
+import service_livraisons_diagramme_p8_fs from "../img/fullstack/p8/cahier_des_charges/service_livraisons_diagramme.png";
+import update_client_p8_fs from "../img/fullstack/p8/cahier_des_charges/update_client.png";
+
+import account_desktop_p6_fs from "../img/fullstack/p6/account_desktop_p6_fs.png";
+import account_phone_p6_fs from "../img/fullstack/p6/account_phone_p6_fs.png";
+import main_desktop_p6_fs from "../img/fullstack/p6/main_desktop_p6_fs.png";
+import main_phone_p6_fs from "../img/fullstack/p6/main_phone_p6_fs.png";
+import post_desktop_p6_fs from "../img/fullstack/p6/post_desktop_p6_fs.png";
+import post_phone_p6_fs from "../img/fullstack/p6/post_phone_p6_fs.png";
+import topics_desktop_p6_fs from "../img/fullstack/p6/topics_desktop_p6_fs.png";
+
+import back_coverage_test_p5_fs from "../img/fullstack/p5/back_coverage_test_p5_fs.png";
+import E2E_coverage_p5_fs from "../img/fullstack/p5/E2E_coverage_p5_fs.png";
+import front_coverage_test_p5_fs from "../img/fullstack/p5/front_coverage_test_p5_fs.png";
+import main_desktop_p5_fs from "../img/fullstack/p5/main_desktop_p5_fs.png";
+
+import banner_p4_fs from "../img/fullstack/p4/banner_p4_fs.png";
+
+import account_p3_fs from "../img/fullstack/p3/account_p3_fs.png";
+import login_p3_fs from "../img/fullstack/p3/login_p3_fs.png";
+import main_desktop_p3_fs from "../img/fullstack/p3/main_desktop_p3_fs.png";
+import register_p3_fs from "../img/fullstack/p3/register_p3_fs.png";
+import rental_desktop_p3_fs from "../img/fullstack/p3/rental_desktop_p3_fs.png";
+import rental_phone_p3_fs from "../img/fullstack/p3/rental_phone_p3_fs.png";
+
+import main_background_desktop_p2_fs from "../img/fullstack/p2/main_background_desktop_p2_fs.png";
+import main_background_phone_p2_fs from "../img/fullstack/p2/main_background_phone_p2_fs.png";
+import country_desktop_p2_fs from "../img/fullstack/p2/country_desktop_p2_fs.png";
+import country_phone_p2_fs from "../img/fullstack/p2/country_phone_p2_fs.png";
+
 import main_background_p14 from '../img/p14/background-p14.jpg';
 import employee_list_p14 from '../img/p14/employee-list.png';
 import login_background_p13 from '../img/p13/login-background-p13.png';
@@ -32,6 +87,225 @@ import home_desk_p2 from '../img/p2/home_desk_p2.png';
 import home_mobile_p2 from '../img/p2/home_mobile_p2.png';
 
 export const PROJECTS = [
+    {
+        id:'your_car_your_way',
+        title:"Your car your way",
+        description:"your_car_your_way",
+        github_repo_link:"https://github.com/rayanadir/poc_13",
+        website_link:"",
+        main_image: [banner_p13_fs],
+        stack: ["Angular", "Typescript", "Spring Boot", "Java", "MySQL"],
+        paragraphers: [
+        "your_car_your_way0",
+        "your_car_your_way1",
+        "your_car_your_way2",
+        "your_car_your_way3",
+        "your_car_your_way4",
+        "your_car_your_way5",
+        ],
+        other_links: {},
+        responsive: false,
+        images: [banner_p13_fs, diagramme_composants_p13_fs, diagramme_deploiement_p13_fs, diagramme_modele_de_donnees_p13_fs],
+    },
+    {
+        id:'maison_en_place',
+        title:"Maison en place",
+        description:"maison_en_place",
+        github_repo_link:"https://github.com/rayanadir/Planifier-des-tests-de-la-surveillance-et-des-backups",
+        website_link:"",
+        main_image: [banner_p12_fs],
+        stack: [],
+        paragraphers: [
+           "maison_en_place0",
+           "maison_en_place1", 
+           "maison_en_place2",
+           "maison_en_place3",
+           "maison_en_place4",
+        ],
+        other_links: {},
+        responsive: false,
+        images: [banner_p12_fs],
+    },
+    {
+        id:'catasterre',
+        title:"Catasterre",
+        description:"catasterre",
+        github_repo_link:"https://github.com/rayanadir/Former-et-encadrer-une-equipe-de-developpement-fullstack",
+        website_link:"",
+        main_image: [banner_p11_fs],
+        stack: ["Notion"],
+        paragraphers: [
+            "catasterre0",
+            "catasterre1",
+            "catasterre2",
+            "catasterre3",
+            "catasterre4",
+            "catasterre5",
+        ],
+        other_links: {},
+        responsive: false,
+        images: [banner_p11_fs],
+    },
+    {
+        id:'bobapp',
+        title:"BobApp",
+        description:"bobapp",
+        github_repo_link:"https://github.com/rayanadir/Gerez-un-projet-collaboratif-en-int-grant-une-demarche-CI-CD",
+        website_link:"",
+        main_image: [banner_p10_fs],
+        stack: ["Docker", "Github Actions", "SonarQube","Angular", "Spring Boot", "Java", "Typescript"],
+        paragraphers: [
+            "bobapp0",
+            "bobapp1",
+            "bobapp2",
+            "bobapp3",
+        ],
+        other_links: {},
+        responsive: false,
+        images: [banner_p10_fs],
+    },
+    {
+        id:'delivery_fit',
+        title:"Delivery Fit",
+        description:"delivery_fit",
+        github_repo_link:"https://github.com/rayanadir/Valider-chiffrer-et-cadrer-un-projet",
+        website_link:"",
+        main_image: [banner_p9_fs],
+        stack: [],
+        paragraphers: [
+            "delivery_fit0",
+            "delivery_fit1",
+            "delivery_fit2",
+        ],
+        other_links: {},
+        responsive: false,
+        images: [banner_p9_fs],
+    },
+    {
+        id:'livrai',
+        title:"Livrai",
+        description:"livrai",
+        github_repo_link:"https://github.com/rayanadir/Realiser-un-cahier-des-charges-fonctionnel-et-technique-pour-une-application",
+        website_link:"",
+        main_image: [banner_p8_fs],
+        stack: ["Angular", "Angular Material", "Spring Boot", "Spring Security", "MySQL", "Docker"],
+        paragraphers: [
+            "livrai0",
+            "livrai1",
+            "livrai2",
+            "livrai3",
+        ],
+        other_links: {
+            "livrai_link_0":"https://github.com/OpenClassrooms-Student-Center/Realisez-un-cahier-des-charges-fonctionnel-et-technique-pour-une-application",
+        },
+        responsive: false,
+        images: [
+            banner_p8_fs, 
+            audit_architecture_p8_fs,
+            audit_clients_p8_fs,
+            audit_deliveries_p8_fs, 
+            audit_login_p8_fs, 
+            audit_logout_p8_fs, 
+            architecture_p8_fs, 
+            authentification_deconnexion_p8_fs, 
+            client_diagramme_p8_fs, 
+            client_p8_fs, 
+            commercial_p8_fs, 
+            livraisons_p8_fs, 
+            login_p8_fs, 
+            register_p8_fs, 
+            service_commercial_diagramme_p8_fs, 
+            service_livraisons_diagramme_p8_fs, 
+            update_client_p8_fs
+        ],
+    },
+    {
+        id:'monde_de_dev',
+        title:"Monde de Dév",
+        description:"monde_de_dev",
+        github_repo_link:"https://github.com/rayanadir/Developpez-une-application-full-stack-complete/tree/develop",
+        website_link:"",
+        main_image: [main_desktop_p6_fs],
+        stack: ["Angular", "Spring Boot", "Spring Security", "Java", "Typescript", "Angular Material", "MySQL"],
+        paragraphers: [
+            "monde_de_dev0",
+            "monde_de_dev1",
+            "monde_de_dev2",
+        ],
+        other_links: {},
+        responsive: true,
+        images: [main_desktop_p6_fs,main_phone_p6_fs,post_desktop_p6_fs,post_phone_p6_fs,topics_desktop_p6_fs,account_desktop_p6_fs, account_phone_p6_fs],
+    },
+    {
+        id:'yoga-app',
+        title:"Yoga App",
+        description:"yoga-app",
+        github_repo_link:"https://github.com/rayanadir/Testez-une-application-full-stack",
+        website_link:"",
+        main_image: [main_desktop_p5_fs],
+        stack: ["Angular", "Spring Boot", "Spring Security", "Typescript", "Java", "Jest","Cypress", "JUnit", "Mockito", "MySQL"],
+        paragraphers: [
+            "yoga-app0",
+            "yoga-app1",
+            "yoga-app2",
+        ],
+        other_links: {},
+        responsive: true,
+        images: [main_desktop_p5_fs, front_coverage_test_p5_fs, back_coverage_test_p5_fs,E2E_coverage_p5_fs],
+    },
+    {
+        id:'knesh',
+        title:"Knesh",
+        description:"knesh",
+        github_repo_link:"https://github.com/rayanadir/Definir-technologies-et-paradigmes-d-une-application",
+        website_link:"",
+        main_image: [banner_p4_fs],
+        stack: ["Angular", "Spring Boot", "Typescript", "Java", "Jest", "Cypress", "JUnit"],
+        paragraphers: [
+            "knesh0",
+            "knesh1",
+        ],
+        other_links: {},
+        responsive: false,
+        images: [banner_p4_fs],
+    },
+    {
+        id:'chatop',
+        title:"ChaTôp",
+        description:"chatop",
+        github_repo_link:"https://github.com/rayanadir/p3_backend_spring",
+        website_link:"",
+        main_image: [main_desktop_p3_fs],
+        stack: ["Angular", "Typescript", "Java", "Spring Boot", "Spring Security", "MySQL", "Swagger"],
+        paragraphers: [
+            "chatop0",
+            "chatop1",
+            "chatop2",
+            "chatop3"
+        ],
+        other_links: {
+            "chatop_link_0":"https://github.com/rayanadir/Developpez-le-back-end-en-utilisant-Java-et-Spring"
+        },
+        responsive: true,
+        images: [main_desktop_p3_fs, rental_desktop_p3_fs, rental_phone_p3_fs, login_p3_fs, register_p3_fs, account_p3_fs],
+    },
+    {
+        id:'olympic_games',
+        title:"Olympic Games",
+        description:"olympic_games",
+        github_repo_link:"https://github.com/rayanadir/Developpez-le-front-end-en-utilisant-Angular",
+        website_link:"",
+        main_image: [main_background_desktop_p2_fs],
+        stack: ["Angular", "Typescript"],
+        paragraphers: [
+            "olympics_games0",
+            "olympics_games1",
+            "olympics_games2",
+        ],
+        other_links: {},
+        responsive: true,
+        images: [main_background_desktop_p2_fs,main_background_phone_p2_fs,country_desktop_p2_fs, country_phone_p2_fs],
+    },
     {
         id:'hrnet',
         title: 'HRNet React',
