d4497ec1970cc3e28ff97a4cdf711732028997e3
rayanadir <rayan.dahmena@gmail.com>
2024-01-11 22:34:40 +0100

    add new stack

--- frontend/src/data/stack.js
+++ frontend/src/data/stack.js
@@ -12,9 +12,77 @@ import npm from "../img/icons/npm.svg";
 import react from "../img/icons/react-icon.svg";
 import redux from "../img/icons/redux.svg";
 import sass from "../img/icons/sass-icon.svg";
-import sqlite from "../img/icons/sqlite.svg"; 
+import sqlite from "../img/icons/sqlite.svg";
+import angular_material from "../img/icons/angular_material.svg";
+import angular from "../img/icons/angular.svg";
+import cypress from "../img/icons/cypress.jpeg";
+import docker from "../img/icons/docker.svg";
+import drawio from "../img/icons/drawio.svg";
+import github_actions from "../img/icons/github_actions.png";
+import intellij_idea from "../img/icons/intellij_idea.svg";
+import java from "../img/icons/java.svg";
+import junit from "../img/icons/junit.svg";
+import maven from "../img/icons/maven.svg";
+import mockito from "../img/icons/mockito.svg";
+import mysql from "../img/icons/mysql.svg";
+import sonarqube from "../img/icons/sonarqube.svg";
+import spring_boot from "../img/icons/spring_boot.svg";
+import spring_security from "../img/icons/spring_security.svg";
+import swagger from "../img/icons/swagger.svg";
+import typescript from "../img/icons/typescript.svg";
+import visual_studio_code from "../img/icons/visual_studio_code.svg";
+import notion from "../img/icons/notion.svg";
+
 
 export const STACK =[
+    {
+        id:"angular",
+        name:"Angular",
+        type:"front",
+        icon: angular
+    },
+    {
+        id:"react",
+        name:"React",
+        type:"front",
+        icon: react,
+    },
+    {
+        id:"js",
+        name:"JS",
+        type:"front",
+        icon: js,
+    },
+    {
+        id:"typescript",
+        name:"Typescript",
+        type:"front",
+        icon: typescript,
+    },
+    {
+        id:"redux",
+        name:"Redux",
+        type:"front",
+        icon: redux,
+    },
+    {
+        id:"cypress",
+        name:"Cypress",
+        type:"front",
+        icon: cypress,
+    },
+    {
+        id:"jest",
+        name:"Jest",
+        type: "front",
+        icon: jest,
+    },
+    {
+        id:"angular_material",
+        name:"Angular Material",
+        type: "front",
+        icon: angular_material
+    },
     {
         id:"html",
         name:"HTML",
@@ -34,34 +102,34 @@ export const STACK =[
         icon: sass,
     },
     {
-        id:"js",
-        name:"JS",
-        type:"front",
-        icon: js,
+        id:"java",
+        name:"Java",
+        type:"back",
+        icon: java,
     },
     {
-        id:"react",
-        name:"React",
-        type:"front",
-        icon: react,
+        id:"spring_boot",
+        name:"Spring Boot",
+        type:"back",
+        icon: spring_boot,
     },
     {
-        id:"redux",
-        name:"Redux",
-        type:"front",
-        icon: redux,
-    },
-    {
-        id:"jest",
-        name:"Jest",
-        type: "front",
-        icon: jest,
+        id:"spring_security",
+        name:"Spring Security",
+        type:"back",
+        icon: spring_security,
     },
     {
         id:"node",
         name:"Node.js",
         type:"back",
         icon: node,
+    },
+    {
+        id:"mysql",
+        name:"MySQL",
+        type:"back",
+        icon: mysql,
     },
     {
         id:"mongo",
@@ -82,10 +150,28 @@ export const STACK =[
         icon: sqlite,
     },
     {
+        id:"junit",
+        name:"JUnit",
+        type:"back",
+        icon: junit,
+    },
+    {
+        id:"mockito",
+        name:"Mockito",
+        type:"back",
+        icon: mockito,
+    },
+    {
         id:"npm",
         name:"NPM",
         type:"other",
         icon: npm
+    },
+    {
+        id:"maven",
+        name:"Maven",
+        type:"other",
+        icon: maven
     },
     {
         id:"git",
@@ -100,9 +186,57 @@ export const STACK =[
         icon: github,
     },
     {
+        id:"docker",
+        name:"Docker",
+        type:"other",
+        icon: docker
+    },
+    {
+        id:"github_actions",
+        name:"Github Actions",
+        type:"other",
+        icon: github_actions
+    },
+    {
+        id:"visual_studio_code",
+        name:"Visual Studio Code",
+        type:"other",
+        icon: visual_studio_code
+    },
+    {
+        id:"intellij_idea",
+        name:"IntelliJ IDEA",
+        type:"other",
+        icon: intellij_idea
+    },
+    {
+        id:"sonarqube",
+        name:"SonarQube",
+        type:"other",
+        icon: sonarqube
+    },
+    {
+        id:"notion",
+        name:"Notion",
+        type:"other",
+        icon: notion
+    },
+    {
         id:"figma",
         name:"Figma",
         type:"other",
         icon: figma,
     },
+    {
+        id:"drawio",
+        name:"Draw.io",
+        type:"other",
+        icon: drawio
+    },
+    {
+        id:"swagger",
+        name:"Swagger",
+        type:"other",
+        icon: swagger,
+    }
 ]
